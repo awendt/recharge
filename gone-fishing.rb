d8ef50822c3bc671082bd6955812b979d6c6654f
André Wendt <awendt@freeshell.org>
2011-01-06 16:29:51 +0100

    Tables to the rescue, and helpers!

--- gone-fishing.rb
+++ gone-fishing.rb
@@ -3,6 +3,23 @@ require 'sinatra'
 set :views, './views'
 set :public, File.dirname(__FILE__) + '/public'
 
+helpers do
+  def calendar_for(year)
+    erb :_calendar, :layout => false, :locals => {:year => year}
+  end
+
+  def weekend?(time)
+    [0,6].include?(time.wday)
+  end
+
+  def id_for(*args)
+    case args.size
+    when 2 then "#{args[0]}#{"%02d" % args[1]}"
+    else "#{args[0]}#{"%02d" % args[1]}#{"%02d" % args[2]}"
+    end
+  end
+end
+
 get '/' do
   erb :index
 end
