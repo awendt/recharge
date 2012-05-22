925d8c0f68613c5f250fa6d2f9bc9165624ce342
André Wendt <awendt@freeshell.org>
2012-05-22 23:05:48 +0200

    Update jQuery to latest version

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,5 +1,5 @@
-//= require jquery-1.4.4
-//= require jquery-ui-1.8.7
+//= require jquery-1.8.3.min
+//= require jquery-ui-1.9.2.min
 //= require jquery.cookie
 //= require ZeroClipboard
 //= require bootstrap-twipsy
