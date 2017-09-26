0d5b3c7ae8705f3ef5f328bac7cc6d31763ffd5a
André Wendt <awendt@freeshell.org>
2017-09-26 22:31:11 +0200

    Configure awscli

--- create_table.sh
+++ create_table.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# irrelevant but required
+export AWS_ACCESS_KEY_ID=foo
+export AWS_SECRET_ACCESS_KEY=bar
+export AWS_DEFAULT_REGION=irrelevant
+
 aws dynamodb create-table \
   --table-name recharge \
   --attribute-definitions AttributeName=UserId,AttributeType=S AttributeName=Year,AttributeType=S \
